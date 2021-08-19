364e83eb65fd47ff0fb09e6df6fecf438097bd50
andrewyang17 <yangzhisiangg@gmail.com>
2021-08-19 11:10:56 -0400

    uncomment

--- util/tensor.go
+++ util/tensor.go
@@ -1,15 +1,20 @@
 package util
 
+import (
+	ts "github.com/sugarme/gotch/tensor"
+	"reflect"
+)
+
 // Equal compares 2 tensors in terms of shape, and every element values.
-//func Equal(tensorA, tensorB *ts.Tensor) bool {
-//	var equal int64 = 0
-//	// 1. Compare shape
-//	if reflect.DeepEqual(tensorA.MustSize(), tensorB.MustSize()) {
-//		// 2. Compare values
-//		equal = tensorA.MustEq1(tensorB, false).MustAll(false).Int64Values()[0]
-//	}
-//	if equal == 0 {
-//		return false
-//	}
-//	return true
-//}
+func Equal(tensorA, tensorB *ts.Tensor) bool {
+	var equal int64 = 0
+	// 1. Compare shape
+	if reflect.DeepEqual(tensorA.MustSize(), tensorB.MustSize()) {
+		// 2. Compare values
+		equal = tensorA.MustEq1(tensorB, false).MustAll(false).Int64Values()[0]
+	}
+	if equal == 0 {
+		return false
+	}
+	return true
+}
